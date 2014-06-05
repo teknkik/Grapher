433731edd789ed5934153275bfeb79de76d389e6
teknkik <riku.karvonen@kapsi.fi>
2014-06-05 12:28:07 +0300

    Update gen_humid.php

--- gen_humid.php
+++ gen_humid.php
@@ -1,6 +1,6 @@
 #!/usr/bin/php5
 <?php #Script for generating humidity graphs with graphs.php
-require_once("graphs.php");
+include("graphs.php");
 genHumidGraph(1600, 800, 5, "humid5", $link);
 genHumidGraph(1600, 800, 15, "humid15", $link);
 ?>
