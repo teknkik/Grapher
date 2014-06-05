421e977743cafd43d7e8eb64d4c91cd46f7967a1
tek <pi@raspberrypi.(none)>
2014-06-05 12:33:46 +0300

    added read_once.py

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Grapher
 =======
 
-A Quick &amp; Dirty SHT1x -sensor compatible graph generator for the Raspberry Pi
+A Quick &amp; Dirty SHT1x -sensor compatible graph generator for the Raspberry Pi. Use readonce.py for crontab and read.py for running as a intependent server
 
 Requires:
 
---
+++ read_once.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import time
+import MySQLdb
+from sht1x.Sht1x import Sht1x as SHT1x
+dataPin = 16
+clkPin = 15
+
+sht1x = SHT1x(dataPin, clkPin, SHT1x.GPIO_BOARD)
+
+db = MySQLdb.connect(host = "localhost", user = "pi", passwd = "rasbian", db = "pi")
+cur = db.cursor()
+cur.execute("SET SESSION time_zone = '+3:00'")
+db.commit()
+	
+temp = round(sht1x.read_temperature_C(),1)
+humidity = round(sht1x.read_humidity(),1)
+
+cur.execute("INSERT INTO lukemat (timestamp,temp,humidity,light,pressure) VALUES(now(),"+str(temp)+","+str(humidity)+",0,0)")
+print("Temperature: "+str(temp)+" C","Humidity: "+str(humidity)+"%")	
+db.commit()
+
